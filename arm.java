d730193f7986ba02d4335e9be7b09e7c6ffa3151
eagms <eagumus@proton.me>
2024-10-08 13:16:57 +0300

    rename commit

--- arm.java
+++ arm.java
@@ -1,3 +1,31 @@
+<<<<<<< HEAD
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Arm extends TimedRobot {
+
+  private final PIDController pid = new PIDController(0.1, 0.0, 0.0);
+  private final PWMSparkMax motorsys = new PWMSparkMax(2);
+  private final Encoder encoder = new Encoder(0, 1);
+
+  private static final double angle = 45.0;
+
+  @Override
+  public void robotInit() {
+    encoder.reset();
+
+    encoder.setDistancePerPulse(360.0 / 4096.0);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    motorsys.set(pid.calculate(encoder.getDistance(), angle));
+  }
+=======
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Encoder;
@@ -24,4 +52,5 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
     motorsys.set(pid.calculate(encoder.getDistance(), angle));
   }
+>>>>>>> fdb62d40b15c4d6c3be54c04ac6349c5ad37ffdf
 }
--- tank.java
+++ tank.java
@@ -1,3 +1,24 @@
+<<<<<<< HEAD
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
+public class Tank extends TimedRobot {
+
+  private final Joystick joystick = new Joystick(0); 
+  private final MotorController rmotor = new PWMSparkMax(1); 
+  private final MotorController lmotor = new PWMSparkMax(0);  
+  private final DifferentialDrive driveTrain = new DifferentialDrive(lmotor, rmotor);
+
+  @Override
+  public void teleopPeriodic() {
+    driveTrain.arcadeDrive(joystick.getY(), joystick.getX());
+  }
+=======
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -17,4 +38,5 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
     driveTrain.arcadeDrive(joystick.getY(), joystick.getX());
   }
+>>>>>>> fdb62d40b15c4d6c3be54c04ac6349c5ad37ffdf
 }
